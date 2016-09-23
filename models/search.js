9c8b33b4a4fcc3cb68f23e471c35ee0d98fa148a
Stephane Beland <Stephane Beland>
2016-09-23 11:29:04 -0700

    fixed a couple bugs in the scoring algorithm

--- models/search.js
+++ models/search.js
@@ -35,37 +35,43 @@ exports.populateSearchParams = function() {
 // compute the match score for each user profile
 
 exports.scoreCalc = function(formParams) {
+	var tempPriceList = exports.populateSearchParams().prices;
+	formParams.setPrice = _.parseInt(formParams.setPrice);
+
 	for (var i = 0; i < testData.length; i++) {
 		var score = 0;
 		
 		if (formParams.setProvider == testData[i].provider) {
-		score += 20;
+			score += 20;
 		}
 		if (formParams.setCountry == testData[i].country) {
-		score += 20;
+			score += 20;
 		}
 		if (formParams.setCategory == testData[i].category_title){
-		score += 20;
+			score += 20;
 		}
 		// handle proportional scoring for rating
+		// score is divided the number of rating points away from setRating
 		if (formParams.setRating == testData[i].ratings) {
-		score += 20;
-		}else {   
-		score += (20 * (1 / (Math.abs(testData[i].ratings - formParams.setRating))) );
+			score += 20;
+		} else {   
+			score += (10 / (Math.abs(testData[i].ratings - formParams.setRating)));
 		}
 		
 		// handle proportional scoring for price as an absolut percentage difference between the two values
-		if ((formParams.setPrice) == testData[i].price_cents){
-		score += 20; 
+		// score is divided the number of index spaces in the price array
+		if ((formParams.setPrice * 100) === testData[i].price_cents){
+			score += 20; 
 		}else {
-		var tempPriceList = exports.populateSearchParams().prices;
-		tempPriceList.push(formParams.setPrice);
-		tempPriceList = tempPriceList.sort(function (a, b) {  return a - b;  });
-		var x = tempPriceList.indexOf(formParams.setPrice);
-		var y = tempPriceList.indexOf(testData[i].price_cents);
-
-		score += (20 * (1 / Math.abs(x - y)));
-		
+			
+			tempPriceList.push(formParams.setPrice * 100);
+			tempPriceList = tempPriceList.sort(function (a, b) {  return a - b;  });
+			
+			var x = tempPriceList.indexOf(formParams.setPrice * 100);
+			var y = tempPriceList.indexOf(testData[i].price_cents);
+			score += (10 / (Math.abs(y - x))); 
+			tempPriceList.splice(x, 1);
+			
 		}
     
     testData[i].score = Math.round(score);
